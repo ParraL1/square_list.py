905c2ac405c9e51d2eece70425cc0e48adf357b1
ParraL1 <97068405+ParraL1@users.noreply.github.com>
2022-02-16 14:37:33 -0600

    Initial commit

---
+++ square_list.py
@@ -0,0 +1,10 @@
+#Name: Lilliana Parra
+#Github username: ParraL1
+#Date: 02/16/2022
+#Description: takes list of numbers and replaces them with their square values.
+
+
+def square_list(list_1):
+    for i in range(len(list_1)):
+        list_1[i] = list_1[i]*list_1[i]
+
